f92cfdc719ccd5234a07881e09f0a14b088b302d
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-14 15:34:20 -0300

    feat(channels/models): classe Marketplace foi adicionada.

--- channels/models.py
+++ channels/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Marketplace(models.Model):
+    name = models.CharField(max_length=100)
+    
+    class Meta:
+        ordering = ['name']
+    
+    def __str__(self):
+        return self.name
