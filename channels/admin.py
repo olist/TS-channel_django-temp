061520bef502b545ca3d54aa2759ed032b4b809d
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-14 15:36:39 -0300

    feat(channels/admin): classe MarketplaceAdmin foi adicionada.

--- channels/admin.py
+++ channels/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+ from django.contrib import admin
+from .models import Marketplace
 
-# Register your models here.
+
+
+@admin.register(Marketplace)
+class MarketplaceAdmin(admin.ModelAdmin):
+    pass
