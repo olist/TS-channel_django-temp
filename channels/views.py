533e77e16360802b171d94272738e43956c74897
matheus <matheus.tessaroli@olist.com>
2021-06-14 16:49:40 -0300

    flake8 in most of files

--- channels/views.py
+++ channels/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from channels.models import ProductPost
 
+
 def list_product(request):
     posted_products = ProductPost.objects.all()
     return render(request, 'products.hmtl', posted_products=posted_products)
 
+
 def create_product(request):
     pass
 
+
 def update_product(request):
     pass
+
 
 def delete_product(request):
     pass
