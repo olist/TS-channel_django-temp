82df3228f4eb7ab31eeac6673c71dec71c02f037
Danilo Costa <danilo.v.barbosa@gmail.com>
2021-06-14 18:20:24 -0300

    feat(templates): list e form templates definidos.

--- channels/models.py
+++ channels/models.py
@@ -22,7 +22,6 @@ class ProductPost(models.Model):
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         managed = True
--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 python manage.py makemigrations
 python manage.py migrate
+# echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
 python manage.py test
 python manage.py runserver 0:3000
