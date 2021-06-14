47e034c61268844ce55b755745984688449073bf
matheus <matheus.tessaroli@olist.com>
2021-06-14 16:25:24 -0300

    form with product post

---
+++ channels/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from channels.models import ProductPost
+
+class ProductPostForm(ModelForm):
+    class Meta:
+        model = ProductPost
+        fields = '__all__'
