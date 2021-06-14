461a1e215c52170c1d191387f0a7b179c1aed321
matheus <matheus.tessaroli@olist.com>
2021-06-14 17:41:26 -0300

    slash on url

--- setup/urls.py
+++ setup/urls.py
@@ -19,5 +19,5 @@ from django.urls import base, path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    path('post-products', include('channels.urls')),
+    path('post-products/', include('channels.urls')),
 ]
