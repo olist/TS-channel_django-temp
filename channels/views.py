14782a303cb2a00ca193041fc8d659fe1bb2de6c
Victor Hugo <victorblog410@gmail.com>
2021-06-14 17:56:00 -0300

    added view and models

--- channels/views.py
+++ channels/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import UserAccount
+
+
+def become_user(request):
+    form = UserCreationForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            user = UserAccount.objects.create(name=user.username, created_by=user)
+            return redirect('login')
+        else:
+            form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
