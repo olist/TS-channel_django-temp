58549167bab2181d312ce86c245968d0e74b9b9a
fernandaformiga <ferformigas@gmail.com>
2021-06-15 09:56:37 -0300

    Create models

--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class OrderLog(models.Model):
+
+    requested_at = models.DateTimeField(auto_now_add=True)
+    responded_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class ShippingLog(models.Model):
+
+    requested_at = models.DateTimeField(auto_now_add=True)
+    responded_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.id)
