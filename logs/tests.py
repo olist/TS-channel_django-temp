73315c89c5ebe2ffc26607b42d3f62b3896a30fd
fernandaformiga <ferformigas@gmail.com>
2021-06-15 10:07:46 -0300

    Create models tests

--- logs/tests.py
+++ logs/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+import pytest
+from .models import OrderLog, ShippingLog
 
 # Create your tests here.
+@pytest.mark.django_db
+def test_order_log_model():
+    order_log = OrderLog.objects.create(status="requested")
+    order_log.save()
+    order_response = "responsed"
+    order_log.status = order_response
+    assert order_log.status == "responsed"
+
+@pytest.mark.django_db
+def test_shipping_log_model():
+    shipping_log = ShippingLog.objects.create(status="requested")
+    shipping_log.save()
+    shipping_response = "responsed"
+    shipping_log.status = shipping_response
+    assert shipping_log.status == "responsed"
